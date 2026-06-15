159d9614f4775ba4133034298cf2dd8d36afd161
Yousef Salah <bhy13565@gmail.com>
2026-06-15 22:29:52 +0300

    add stream debug routes

--- server/stream-test.js
+++ server/stream-test.js
@@ -154,8 +154,9 @@ app.get("/proxy", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5001;
+const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Stream proxy running on port ${PORT}`);
+app.use((req, res, next) => {
+  console.log("REQ:", req.method, req.url);
+  next();
 });
